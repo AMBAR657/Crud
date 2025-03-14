4c6ad7f9b66f159648b08819361ed9d48892d12c
Ambar Itzel <zarzaambar52@gmail.com>
2025-03-13 21:07:06 -0600

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         await mongoose.connect('mongodb://localhost:27017/tu_base_de_datos');
+        //Manda el mensaje si la conexion a la base de datos fue correcta
         console.log('Conexión exitosa a la base de datos');
     } catch (error) {
         console.error('Error al conectar a la base de datos:', error.message);
